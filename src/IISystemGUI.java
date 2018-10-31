c12a608f8990b5e52186b60efd3c02f65b29aff2
Jinsoo Choi <jinsoo@Jinsooui-MacBook-Pro.local>
2018-10-31 13:26:51 -0700

    implement GUI exec

--- src/IISystemGUI.java
+++ src/IISystemGUI.java
@@ -1,3 +1,14 @@
-public class IISystemGUI {
+import javax.swing.*;
 
+public class IISystemGUI {
+    private static IISystem iis;
+    private static JFrame iisframe;
+
+    public static void main(String[] args) throws Exception {
+        iis = new IISystem();
+        iisframe = new IISystemFrame(iis);
+
+        iisframe.setLocationRelativeTo(null);
+        iisframe.setVisible(true);
+    }
 }
