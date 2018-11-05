c28065edede1fa76a5c64f3183de2c0ccef57b53
Jinsoo Choi <jinsoo@Jinsooui-MacBook-Pro.local>
2018-11-05 14:35:39 -0800

    add comments on IISystemGUI.java

--- src/IISystemGUI.java
+++ src/IISystemGUI.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
 
+/*
+ * the main program to run the GUI for IISystem
+ */
 public class IISystemGUI {
     private static IISystem iis;
     private static JFrame iisframe;
 
+    // main function to run the program
     public static void main(String[] args) throws Exception {
         iis = new IISystem();
         iisframe = new IISystemFrame(iis);
